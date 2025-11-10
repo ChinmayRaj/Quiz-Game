2edbc572462d7016cf9bf62b9371bffea257c5d8
ChinmayRaj <chinmayshah2002@gmail.com>
2025-11-10 22:42:09 +0530

    commiting the changes for the different answer type

--- QuizGame/QuestionService.java
+++ QuizGame/QuestionService.java
@@ -9,6 +9,7 @@ public class QuestionService {
         questions[2]=new Question(3,"What is the capital of UK?","Manchester","Birmingham","London","Liverpool","London");
         questions[3]=new Question(4,"What is the capital of Australia?","Sydney","Melbourne","Canberra","Brisbane","Canberra");
         questions[4]=new Question(5,"What is the capital of Canada?","Toronto","Vancouver","Ottawa","Montreal","Ottawa");
+
     }
 
     public void displayQuestion() {
@@ -16,6 +17,7 @@ public class QuestionService {
 int c=0;
 Scanner sc=new Scanner(System.in);
 try{
+    
          for(Question q:questions) {
             
               System.out.println(q.getId()+". "+q.getQuestion());
@@ -24,13 +26,21 @@ System.out.println("Options:");
                 System.out.println("2. "+q.getO2());
                 System.out.println("3. "+q.getO3());
                 System.out.println("4. "+q.getO4());
-                System.out.println("Enter your answer: ");
+                System.out.println("Enter your answer: (Enter the option number only)");
                 
-                String userAnswer=sc.nextLine().trim();
-if(userAnswer.equalsIgnoreCase(q.getAnswer())){ // correct comparison
-                   c++;
-               }
-    }
+//                 String userAnswer=sc.nextLine().trim();
+// if(userAnswer.equalsIgnoreCase(q.getAnswer())){ // correct comparison
+//                    c++;
+//                }
+int userAnswer=sc.nextInt();
+
+if((userAnswer==1 && q.getO1().equals(q.getAnswer())) ||
+   (userAnswer==2 && q.getO2().equals(q.getAnswer())) ||
+   (userAnswer==3 && q.getO3().equals(q.getAnswer())) ||
+   (userAnswer==4 && q.getO4().equals(q.getAnswer()))){
+       c++;
+   }
+         }
 }
 finally {
            sc.close();
